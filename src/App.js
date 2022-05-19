38728316363f15ee249d777b58ca413ddcc5bba7
mubariz <syedmubariz542@gmail.com>
2022-05-19 18:02:01 +0530

    react header footer app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header';
+import Footer from './Footer'
 
 function App() {
-    return (<div>
+  return (<div>
+      <Header/>
         <h1>hello world</h1>
         <img className='cars'src='https://media.wired.com/photos/5d09594a62bcb0c9752779d9/1:1/w_1500,h_1500,c_limit/Transpo_G70_TA-518126.jpg'></img>
+        <Footer />
     </div>);
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return (<div>
+            cars.com<br></br>
+            @api 2022
+        </div>);
+    }
+}
+ 
+export default Footer;
