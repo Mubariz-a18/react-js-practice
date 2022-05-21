5f98ce663a2cb9c65c83871a064aefc7117dd981
mubariz <syedmubariz542@gmail.com>
2022-05-22 00:11:31 +0530

    list rendering using map function

--- src/utils/List.js
+++ src/utils/List.js
@@ -5,13 +5,9 @@ function List(){
     var array = [1,2,3,4,5,6];
 
     return <div>
-        <ul>
-            <li>{array[0]}</li>
-            <li>{array[2]}</li>
-            <li>{array[3]}</li>
-            <li>{array[4]}</li>
-            <li>{array[5]}</li>
-        </ul>
+   {array.map((i)=>
+   <li>{i}</li>
+       )}
     </div>
 }
 
