7c78720c1bca43c06922ace32786900faad5f1c0
mubariz <syedmubariz542@gmail.com>
2022-05-26 17:43:49 +0530

    refactoring user list with loader and should render

--- src/GitHubApi/UserList.js
+++ src/GitHubApi/UserList.js
@@ -1,24 +1,35 @@
 import axios from 'axios';
 import React from 'react';
 import UserDesign from './UserDez';
+import Loader from '../image/loader.gif'
+import ShouldRender from '../utils/ShouldRender'
 
 class UserList extends React.Component {
     state = {
-        users: []
+        users: [],
+        loading: true
     }
     constructor() {
         super()
         axios.get("https://api.github.com/users").then((res) => {
             const data = res.data
-            this.setState({users: data})
+            this.setState({users: data, loading: false})
         }).catch(err => console.log(err))
     }
     render() {
-        return <>{
+        return <div> 
+        < ShouldRender cond = {this.state.loading}>
+         <div className='loader'>
+                <img width='120px' height='50px'
+                    src={Loader}/>loading...
+            </div>
+            </ShouldRender>
+        
+            {
             this.state.users.map((user) => {
                 return <UserDesign user={user}/>
             })
-        }</>
+        }</div>
     }
 }
 
