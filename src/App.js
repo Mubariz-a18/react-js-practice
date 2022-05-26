35b0464178ccf5fe4cd1a4802db79a529a001bc3
mubariz <syedmubariz542@gmail.com>
2022-05-26 19:36:29 +0530

    not found component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import About from './About';
 import UserList from './GitHubApi/UserList'
 import Contact from './Contact';
+import NotFound from './NotFounnd';
 
 
 
@@ -22,7 +23,10 @@ function App() {
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/users' element={<UserList/>}></Route>
         {/* <Route path='*' element={<Home/>}></Route> */}              
-        <Route path='*' element={<Navigate to ='/' />}></Route>
+        {/* <Route path='*' element={<Navigate to ='/' />}></Route> */}
+        <Route path='*' element={<NotFound/>}></Route>
+
+
         </Routes>
         </BrowserRouter>
          <Footer/>
