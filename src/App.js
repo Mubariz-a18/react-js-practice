9074d04f50b5632044600b4af6b941eb08f3d5a1
mubariz <syedmubariz542@gmail.com>
2022-05-26 19:31:10 +0530

    react router dom
    used to set routes and create end points

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 import Footer from './Footer'
-import UserList from './GitHubApi/UserList'
+import Home from './Home'
 import 'font-awesome/css/font-awesome.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import About from './About';
+import UserList from './GitHubApi/UserList'
+import Contact from './Contact';
+
+
 
 function App() {
     return ( 
         <div>
             <Header/>
-            <UserList/>
-            <Footer/>
+        <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/users' element={<UserList/>}></Route>
+        {/* <Route path='*' element={<Home/>}></Route> */}              
+        <Route path='*' element={<Navigate to ='/' />}></Route>
+        </Routes>
+        </BrowserRouter>
+         <Footer/>
         </div>
     );
 }
